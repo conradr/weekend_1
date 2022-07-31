604e8fca10d3182b8c51b6764880da670959fc19
Conrad Russo <conradr@gmail.com>
2022-07-31 12:11:31 +0100

    pda

---
+++ lists_functions.py
@@ -0,0 +1,24 @@
+friends = [
+    {
+        "name": "Jack",
+        "console": "megadrive",
+        "games": ["Sonic", "Alex the Kid", "Wonderboy"]
+    },
+    {
+        "name": "Sarah",
+        "console": "megadrive",
+        "games": ["Gradius", "Rainbow Islands"]
+    },
+    {
+        "name": "Archie",
+        "console": "pc",
+        "games": ["Doom", "Command and Conquer"]
+    },
+    {
+        "name": "Tony",
+        "console": "pc",
+        "games": ["Age of Empires", "Willy Beamish", "Dune"]
+    }
+]
+
+print(friends)
